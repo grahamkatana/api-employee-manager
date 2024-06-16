500c5f1712ee89c4ea4639722a58d576b4130667
grahamkatana <graykatanakenny@gmail.com>
2024-06-16 18:43:42 +0200

    Build file

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-npm install typescript
+npm install -D ts-node
+npm install -D typescript
+
+ts-node script.ts
 # install packages
 npm install
 # build
